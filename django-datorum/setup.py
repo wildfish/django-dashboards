783fc38d2132912649367f81e1ea9b5c5911f012
James Outterside <james.outterside@gmail.com>
2022-08-18 15:38:42 +0100

    Drop 3.8 for now

--- django-datorum/setup.py
+++ django-datorum/setup.py
@@ -22,7 +22,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Framework :: Django",
