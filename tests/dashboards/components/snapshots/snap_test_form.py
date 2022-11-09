58bff85d874cb33043d75c7366f3aa1176b75cb2
John Peterson <john@wildfish.com>
2022-11-09 11:59:16 +0000

    fix snapshot test for forms

--- tests/dashboards/components/snapshots/snap_test_form.py
+++ tests/dashboards/components/snapshots/snap_test_form.py
@@ -16,21 +16,14 @@ snapshots[
     
     <div id="component-test-inner" class="dashboard-component-inner fade-in">
         <form hx-get="/app1/testdashboard/test-form/?key=test" hx-trigger="change" hx-swap="outerHTML" hx-target="#component-test-inner">
-  <p>
-    <label for="id_number">Number:</label>
-    <select name="number" id="id_number">
+  <p><label for="id_number">Number:</label><select name="number" id="id_number">
   <option value="one">one</option>
 
   <option value="two">two</option>
 
   <option value="three">three</option>
 
-</select>
-    
-    
-      
-    
-  </p>
+</select></p>
 </form>
     </div>
 """
@@ -44,21 +37,14 @@ snapshots[
     
     <div id="component-test-inner" class="dashboard-component-inner fade-in">
         <form hx-get="/app1/testdashboard/test-form/?key=test" hx-trigger="change" hx-swap="outerHTML" hx-target="#component-test-inner">
-  <p>
-    <label for="id_number">Number:</label>
-    <select name="number" id="id_number">
+  <p><label for="id_number">Number:</label><select name="number" id="id_number">
   <option value="one">one</option>
 
   <option value="two">two</option>
 
   <option value="three">three</option>
 
-</select>
-    
-    
-      
-    
-  </p>
+</select></p>
 </form>
     </div>
 """
