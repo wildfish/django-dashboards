044befe33a0f74c121dcb168d90331ed6b2865a9
John Peterson <john@wildfish.com>
2023-01-04 17:01:38 +0000

    isort .

--- wildcoeus/dashboards/component/chart/serializers.py
+++ wildcoeus/dashboards/component/chart/serializers.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Callable
+from typing import Any, Callable, Dict, List, Optional
 
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import QuerySet
